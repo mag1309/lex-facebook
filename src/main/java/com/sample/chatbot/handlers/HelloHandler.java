6581fc2d8ade55548c161fae437a02fe3d39b949
manish <manish@respond.com>
2018-06-09 19:09:04 +0530

    first commit

--- src/main/java/com/sample/chatbot/handlers/HelloHandler.java
+++ src/main/java/com/sample/chatbot/handlers/HelloHandler.java
@@ -1,12 +1,19 @@
 package com.sample.chatbot.handlers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,6 +24,8 @@ import com.ai.chatbot.framework.response.action.CloseDialogAction;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sample.chatbot.framework.handlers.AbstractLexRequestHandler;
 
 
@@ -38,18 +47,29 @@ public class HelloHandler extends AbstractLexRequestHandler implements RequestSt
             
             if(lexRequest != null)
             {
-            	// Get session
+            	Map<String,String> requestAttributes = null;
             	Map<String,String> sessionAttributes = null;        	
+            	
+             	// Get session      
             	if(lexRequest.getSessionAttributes() != null)
             		sessionAttributes = new HashMap<>(lexRequest.getSessionAttributes());
             	else
             		sessionAttributes = new HashMap<>();
             	
-            	// Pull attribute name from Request
-            	String firstName = lexRequest.getCurrentIntent().getSlots().get("firstname");
+            	if(lexRequest.getRequestAttributes() != null)
+            	{
+            		requestAttributes = lexRequest.getRequestAttributes();
+            	   	String pageAccessToken = requestAttributes.get("x-amz-lex:facebook-page-id");
+            	   	String pageScopeId = requestAttributes.get("x-amz-lex:user-id");
+            	   	if(getObjectFromSession(sessionAttributes, "UserID", new TypeReference<String>() {}) == null)
+            	   	{
+            	   		saveObjectIntoSession(sessionAttributes, "UserID", pageScopeId, new TypeReference<String>() {});
+            	   		setUserPublicProfile(sessionAttributes, pageScopeId, pageAccessToken);
+            	   	}
+            	}
             	
-            	// Store is session for future use
-            	saveObjectIntoSession(sessionAttributes,"firstname", firstName, new TypeReference<String>() {});
+            	// Pull First Name from Session
+            	String firstName = getObjectFromSession(sessionAttributes, "FirstName", new TypeReference<String>() {});
             	
             	// Create Response
             	LexResponse lexResponse = new LexResponse(new CloseDialogAction("Fulfilled", new Message("PlainText","Hello "+ firstName + ", Hope your are doing great")),sessionAttributes);
@@ -66,5 +86,54 @@ public class HelloHandler extends AbstractLexRequestHandler implements RequestSt
         outputStream.write(responseBytes);
     }  
 	
-	
+	/**
+	 * 
+	 * @param sessionAttributes
+	 * @param id
+	 * @param token
+	 */
+	protected void setUserPublicProfile(Map<String,String> sessionAttributes, String id, String token) 
+    {
+    	logger.info("id:" + id + "toke:" + token);
+    	String url = "https://graph.facebook.com/v3.0/?ids="+id+"&access_token="+token;
+		
+		try {
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet(url);
+
+			HttpResponse response = client.execute(request);
+			
+			if(response.getStatusLine().getStatusCode() == 200)
+			{
+				BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+				String inputLine;
+				StringBuffer result = new StringBuffer();
+
+				while ((inputLine = in.readLine()) != null) {
+					result.append(inputLine);
+				}
+				in.close();
+				
+				if (logger.isDebugEnabled()) {
+                    logger.debug("Result String :\n {}", result.toString());
+                }
+				
+				JsonNode rootNode = new ObjectMapper().readTree(new StringReader(result.toString()));
+				JsonNode innerNode = rootNode.get(id); // Get the only element in the root node
+			    
+				// get an element in that node
+			    String first_name = innerNode.get("first_name").asText();
+			    String last_name = innerNode.get("last_name").asText();
+				logger.info("first_name:" + first_name + "last_name:" + last_name);
+				saveObjectIntoSession(sessionAttributes, "FirstName", first_name, new TypeReference<String>() {});
+				saveObjectIntoSession(sessionAttributes, "LastName", last_name, new TypeReference<String>() {});
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.info("Exception : " + e.getMessage());
+			throw new RuntimeException(e);
+			//return new LexResponse(new CloseDialogAction("Failed", new Message("PlainText",getMessage("error.message"))));
+		} 
+   	}
 }
